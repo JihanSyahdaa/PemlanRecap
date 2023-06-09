3688eb6acb9c8b5f94d8e106dbd8bb6027668649
JihanSyahdaa <136085904+JihanSyahdaa@users.noreply.github.com>
2023-06-09 22:02:16 +0700

    Update KueJadi.java

--- PEMROGRAMAN_LANJUT/BAB_7/KueJadi.java
+++ PEMROGRAMAN_LANJUT/BAB_7/KueJadi.java
@@ -1,23 +1,5 @@
 package POLIMORFISME;
 
-// public class KueJadi extends Kue {
-//     private double jumlah;
-
-//     public KueJadi(String nama, double harga, double jumlah) {
-//         super(nama, harga);
-//         this.jumlah = jumlah;
-//     }
-
-//     public double getJumlah() {
-//         return jumlah;
-//     }    
-
-//     @Override
-//     public double hitungHarga() {
-//         return harga * jumlah * 2;
-//     }
-// }
-
 class KueJadi extends Kue {
     private int jumlah;
 
