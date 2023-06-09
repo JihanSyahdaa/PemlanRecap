d1611a6e8963fb6976f715746b9367e67ffffee6
JihanSyahdaa <136085904+JihanSyahdaa@users.noreply.github.com>
2023-06-09 22:02:35 +0700

    Update KuePesanan.java

--- PEMROGRAMAN_LANJUT/BAB_7/KuePesanan.java
+++ PEMROGRAMAN_LANJUT/BAB_7/KuePesanan.java
@@ -1,23 +1,5 @@
 package POLIMORFISME;
 
-// public class KuePesanan extends Kue {
-//     private double berat;
-
-//     public KuePesanan(String nama, double harga, double berat) {
-//         super(nama, harga);
-//         this.berat = berat;
-//     }
-
-//     public double getBerat() {
-//         return berat;
-//     }    
-
-//     @Override
-//     public double hitungHarga() {
-//         return harga * berat;
-//     }
-// }
-
 class KuePesanan extends Kue {
     private double berat;
 
