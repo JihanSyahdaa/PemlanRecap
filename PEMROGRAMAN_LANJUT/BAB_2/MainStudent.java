ecb98e1cd607ce3b4dabb12d5bc915497abad0b6
JihanSyahdaa <136085904+JihanSyahdaa@users.noreply.github.com>
2023-06-09 21:58:05 +0700

    Update MainStudent.java

--- PEMROGRAMAN_LANJUT/BAB_2/MainStudent.java
+++ PEMROGRAMAN_LANJUT/BAB_2/MainStudent.java
@@ -1,46 +1,5 @@
 package PRAKTIKUM;
 
-// public class MainStudent {
-//     public static void main(String[] args) {
-//     Student anna = new Student();
-//     anna.setName("Anna");
-//     anna.setAddress("Malang");
-//     anna.setAge(20);
-//     anna.setMath(100);
-//     anna.setScience(89);
-//     anna.setEnglish(80);
-//     anna.displayMessage();
-    
-//      //menggunakan constructor lain
-//      System.out.println("===================");
-//      Student chris = new Student("Chris", "Kediri", 21);
-//      chris.setMath(70);
-//      chris.setScience(60);
-//      chris.setEnglish(90);
-//      chris.displayMessage();
-    
-//      //siswa denagan nama chris dirubah informasi alamat dan     umurnya melalui method
-//      System.out.println("===================");
-//      chris.setAddress("Surabaya");
-//      chris.setAge(22);
-//      chris.displayMessage();
-
-//      Student louis = new Student(90, 70, 50);
-//      louis.setName("louis");
-//      louis.setAddress("Jakarta");
-//      louis.setAge(25);
-//      louis.displayMessage();
-
-//      Student peter = new Student(90, 70, 50);
-//      peter.setName("peter");
-//      peter.setAddress("Bogor");
-//      peter.setAge(23);
-//      peter.displayMessage();
-
-//      }
-     
-//     }
-
 import java.util.Scanner;
 
 public class MainStudent {
